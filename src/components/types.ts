3e61c9b7f17478fe30409d35146f52d1d54f8abd
ShalomKumer <kumersb@gmail.com>
2025-09-17 10:28:52 +0300

    creating the types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,17 +1,37 @@
 export type Book = {
-
-}
-
-export type Average = {
-
+    id:number
+    bookName:string,
+    author:{
+        firstName:string,
+        lastName: string,
+        birthYear?: number,
+    },
+    year:number,
+    read:boolean,
+    genres:string[],
+    reting:{
+        average:number,
+        reviews:string[]
+    }
 }
 
 export type Review = {
-
+    user: string,
+    scor: 1|2|3|4|5
+    comment: string,
 }
 
 export type Member = {
-
+    id:number,
+    firstName: string,
+    lastName: string,
+    age?: number,
+    birthDay:string,
+    borrowedBooks:[{
+        bookId:number,
+        borrowedDate:string,
+    }],
+    booksRead:string[]
 }
 
 
